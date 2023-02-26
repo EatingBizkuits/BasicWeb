1baabb498c4b4a9bf8ea3b47a618cbb12fc0276e
EatingBizkuits <92772930+EatingBizkuits@users.noreply.github.com>
2023-02-27 00:13:47 +0800

    feat: basic setup and refresher to vue

---
+++ basicweb/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <Registration />
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Registration from './components/Registration.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    Registration
+  }
+}
+</script>
+
+<style>
+html, body {
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
+}
+
+
+body {
+  background-image: url("./assets/bg.png");
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+}
+</style>
---
+++ basicweb/src/components/Registration.vue
@@ -0,0 +1,102 @@
+<template>
+    <section class="grid-container">
+        <div class="grid bg-red"></div>
+        <div class="grid"> 
+            <form class="">
+                <h1>Hello <span class="deco-name">{{displayUser}},</span> Sign In</h1>
+                <input type="text" placeholder="Username" v-model="user" required="" maxlength="30">
+                <input type="password" placeholder="Password" required>
+                <input type="submit">
+                <sub>Don't Have an account? Sign Up <a class="hyperlink">here</a></sub>
+            </form>
+        </div>
+    </section>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'RegistrationVue',
+  data() {
+    return {
+        user : "",
+        displayUser: "user"
+    }
+  },
+  methods: {
+    // Your functions
+  },
+  created() {
+    // Start event (do something when started)
+  },
+  updated() {
+    if (this.user == "")
+    {
+        this.displayUser = "user";
+    }
+    else {
+        if (this.user.length < 15) {
+            this.displayUser = this.user
+        }
+        else {
+            this.displayUser = this.user.slice(0, 15) + "...";
+        }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only (local styles) -->
+<style scoped>
+* {
+    color: white;
+}
+
+input {
+    width: 70%;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid black;
+    padding: 5px 15px;
+    margin: 10px auto;
+    color: black;
+}
+
+.grid-container {
+    display: grid;
+    height: 100vh;
+    grid-template-columns: 50vw 50vw;
+}
+
+.grid {
+    height: 100%;
+    display: flex;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+    background-color: rgba(128, 128, 128, 0.452);
+    width: 50%;
+    aspect-ratio: 2/3;
+    margin: auto;
+    padding: 10px;
+    border-radius: 5px;
+}
+
+.deco-name {
+    color: yellow;
+    text-decoration: underline;
+    font-size: 14pt;
+}
+
+sub {
+    margin: 5px auto;
+}
+
+.hyperlink {
+    color: blue;
+    text-decoration: underline;
+    cursor: pointer;
+}
+</style>
